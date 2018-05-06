2e6b54bab8a3d54a810f4094ec9574dde21e1ce3
schlagercollin <schlagercollin@gmail.com>
2018-05-05 22:39:58 -0700

    Mageck implemented. Not seamless

--- screen_analysis_BP.py
+++ screen_analysis_BP.py
@@ -10,6 +10,7 @@ import statsmodels.sandbox.stats.multicomp as compTool
 import argparse
 import threading
 import copy
+import subprocess
 #import annotate_functions
 
 #this method creates the dictionaries that map guide sequence to number to reads and guide sequence to gene name
@@ -52,129 +53,6 @@ def createDictionaries(input_file):
 GUIDE_START = 0 #start index of guide sequence
 GUIDE_END = 20 #end index of guide sequence
 BEFORE_GUIDE = "CACCG" #identifies sequence after guide
-
-#matches the reads to the guides and prints statistics regarding the library. Returns the number of perfectly matched reads
-def count_spacers(fastq_file, output_file, countDict, guideGeneDict, geneCountDict):
-    """
-    creates a dictionary with guide counts from fastq_file, writes to output_file
-    fastq_file: forward read fastq file
-    output_file: csv file to write guide dictionary to
-    dictionary: guide sequence as key, guide count as entry
-    """
-
-    num_reads = 0 #total number of reads processed
-    perfect_matches = 0 # guides with perfect match to library
-    non_perfect_matches = 0 #number of guides without a perfect match to the library
-    perfMatchList = []; #array to hold sequences that perfectly match to library
-    nonPerfList = []; #array to hold sequences that do not perfectly match to the library
-
-    # open fastq file
-    try:
-        handle = open(fastq_file, "rU")
-    except:
-        print("could not find fastq file")
-        return
-
-    # process reads in fastq file
-    readiter = SeqIO.parse(handle, "fastq")
-    for record in readiter: #contains the seq and Qscore etc.
-        sys.stdout.write("\r%d" % num_reads)
-        sys.stdout.flush()
-        num_reads += 1
-        read_sequence = str.upper(str(record.seq))
-        guide = read_sequence[GUIDE_START:GUIDE_END] #get sgRNA sequence from read
-        if guide in countDict: #only count reads that match a guide sequence exactly
-            countDict[guide] += 1; #add one to count for guide
-            perfect_matches += 1;
-            gene = guideGeneDict[guide]; #find gene associated with guide
-            geneCountDict[gene] += 1; #add one to count for gene
-            perfMatchList.append(read_sequence); #add read to perfect-match list
-        else:
-            non_perfect_matches += 1;
-            nonPerfList.append(read_sequence);  #add read to non-perfect match list
-    numGuidesOverHund = 0;
-    # create ordered dictionary with guides and respective counts based on read counts and output as a csv file
-    dict_sorted = OrderedDict(sorted(countDict.items(), key=lambda t: t[1]));
-    outputName = str(output_file+"_guide_count.csv")
-    with open(outputName, 'w') as csvfile:
-        mywriter = csv.writer(csvfile, delimiter=',');
-        mywriter.writerow(["Gene Name", "sgRNA", "Read Count", "Fraction of Total Reads"]);
-        runningSum = 0.0;
-        for guide in dict_sorted:
-            count = dict_sorted[guide];
-            if count >= 100:
-                numGuidesOverHund += 1;
-            gene = guideGeneDict[guide];
-            runningSum += count;
-            fraction = runningSum/perfect_matches;
-            mywriter.writerow([gene, guide, count, fraction]);
-    #print perfect-matches to an output file
-    outputName = str(output_file+"_perfect_matches.csv");
-    with open(outputName, 'w') as csvfile:
-        mywriter = csv.writer(csvfile, delimiter=',');
-        for seq in perfMatchList:
-            mywriter.writerow([seq]);
-    #print non-perfect matches to an output file
-    outputName = str(output_file+"_not_matched.csv");
-    with open(outputName, 'w') as csvfile:
-        mywriter = csv.writer(csvfile, delimiter=',');
-        for seq in nonPerfList:
-            mywriter.writerow([seq]);
-    print("Printed perfectly matched and not-matched sequences");
-
-    # percentage of guides that matched perfectly
-    percent_matched = round(perfect_matches/float(perfect_matches + non_perfect_matches) * 100, 1)
-    # percentage of undetected guides with no read counts
-    guides_with_reads = np.count_nonzero(list(countDict.values()))
-    print(len(list(countDict.values())))
-    guides_no_reads = len(list(countDict.values())) - guides_with_reads
-    percent_no_reads = round(guides_no_reads/float(len(list(countDict.values()))) * 100, 1);
-    #percent of genes with no read counts
-    numGenes = len(list(geneCountDict.values()));
-    genes_with_reads = np.count_nonzero(list(geneCountDict.values()));
-    genes_no_reads = numGenes - genes_with_reads;
-    percent_genes_no_reads = round(genes_no_reads/float(numGenes) * 100, 1);
-    # skew ratio of top 10% to bottom 10% of guide counts
-    top_10 = np.percentile(list(countDict.values()), 90)
-    bottom_10 = np.percentile(list(countDict.values()), 10)
-    if top_10 != 0 and bottom_10 != 0:
-        skew_ratio = top_10/bottom_10
-    else:
-        skew_ratio = 'Not enough perfect matches to determine skew ratio'
-    #percent of guides with more than 100 reads
-    percent_guides_greater_hund = numGuidesOverHund / float(len(countDict.keys())) *100;
-
-    # write analysis statistics to statistics.txt
-    outputName = output_file+"_statistics.txt";
-    with open(outputName, 'w') as infile:
-        infile.write('Number of perfect read matches: ' + str(perfect_matches) + '\n')
-        infile.write('Number of nonperfect read matches: ' + str(non_perfect_matches) + '\n')
-        #infile.write('Number of reads where key was not found: ' + str(key_not_found) + '\n')
-        infile.write('Number of reads processed: ' + str(num_reads) + '\n')
-        infile.write('Percentage of reads that matched perfectly: ' + str(percent_matched) + '\n')
-        infile.write('Percentage of undetected guides: ' + str(percent_no_reads) + '\n')
-        infile.write('Skew ratio of top 10% to bottom 10%: ' + str(skew_ratio) + '\n\n')
-        infile.write('Number of guides with more than 100 reads: '+str(numGuidesOverHund)+'\n');
-        infile.write('Percent of guides with more than 100 reads: '+str(percent_guides_greater_hund)+'\n');
-        infile.write('Percent of genes with no reads: '+str(percent_genes_no_reads)+'\n');
-
-    handle.close()
-
-    # print stats out to *.csv
-    outputName = str(output_fie+"_statistics.csv")
-    with open(outputName, 'w') as csvfile:
-        mywriter = csv.writer(csvfile, delimiter=',');
-        mywriter.writerow(["Number of perfect read matches", perfect_matches]);
-        mywriter.writerow(["Number of nonperfect read matches", non_perfect_matches]);
-        mywriter.writerow(["Number of reads processed", num_reads]);
-        mywriter.writerow(["Percentage of reads that matched perfectly", percent_matched]);
-        mywriter.writerow(["Percentage of undetected guides", percent_no_reads]);
-        mywriter.writerow(["Skew ratio of top 10% to bottom 10%", skew_ratio]);
-        mywriter.writerow(["Number of guides with more than 100 reads", numGuidesOverHund]);
-        mywriter.writerow(["Percent of guides with more than 100 reads", percent_guides_greater_hund]);
-        mywriter.writerow(["Percent of genes with no reads", percent_genes_no_reads]);
-        print("Printed Gene Enrichment stats")
-    return perfect_matches;
 
 #print reads counts per guide and per gene for each population
 def geneReadCounts(outputFileName, geneCountDict, geneToGuideDict, countDict, perfect_matches):
@@ -246,47 +124,6 @@ class Gene:
         #fold change
         self.foldChange = fold_change;
 
-#extract gene names and read counts from unsorted and sorted populations, and perform Fisher's exact test to determine enrichment p-value
-def calcGeneEnrich(unsortedGeneCountDict, unsortedTotMatches, sortedGeneCountDict, sortedTotMatches, descriptionDict):
-    geneStatsDict = {}; #new dictionary to hold Gene objects
-    enrichPVals = [];
-    total_keys = len(unsortedGeneCountDict.keys())
-    keys = unsortedGeneCountDict.keys();
-    keys_total = len(keys)
-    print(keys_total)
-    for counter, key in enumerate(keys):
-        sys.stdout.write("\r%d" % counter)
-        sys.stdout.flush()
-        #make new gene object with the correct counts
-        unsorted_count = int(unsortedGeneCountDict[key])
-        sorted_count = int(sortedGeneCountDict[key])
-        fold_change = math.log2(((sorted_count+1)/sortedTotMatches)/((unsorted_count+1)/unsortedTotMatches))
-        print(unsorted_count, sorted_count, fold_change)
-        gene = Gene(key, unsortedGeneCountDict[key], unsortedTotMatches, sortedGeneCountDict[key], sortedTotMatches, fold_change, descriptionDict[key]["Summary"], descriptionDict[key]["Description"]);
-        geneStatsDict[key] = gene;
-        oddsratio, pValue = stats.fisher_exact([[gene.unsortedReads, gene.unsortedTotal], [gene.sortedReads, gene.sortedTotal]]);
-        gene.enrichPVal = pValue;
-        enrichPVals.append(pValue);
-    #get FDR-corrected pvalues
-    adjEnrichPVals = compTool.multipletests(enrichPVals, method='fdr_bh', is_sorted=False)[1];
-    for i, key in enumerate(keys):
-        gene = geneStatsDict[key];
-        gene.adjEnrichPVal = adjEnrichPVals[i];
-
-    return geneStatsDict;
-
-#prints the enrichment p-values for each gene to a .csv file. The file is ranked from lowest FDR-corrected p-value to highest
-def printGeneEnrichStats(geneStatsDict, output_file):
-    genesList = list(geneStatsDict.values());
-    genesList.sort(key = lambda gene: gene.adjEnrichPVal);
-    outputName = str(output_file+"_gene_enrichment_calculation.csv");
-    with open(outputName, 'w') as csvfile:
-        mywriter = csv.writer(csvfile, delimiter=',');
-        mywriter.writerow(["Gene Name", "FDR-corrected p-value", "p-value", "Reads in Unsorted Population", "Total Reads in Unsorted Population", "Reads in Sorted Population", "Total Reads in Sorted Population","Fold Change", "Summary","Description"]);
-        for gene in genesList:
-            mywriter.writerow([gene.name, gene.adjEnrichPVal, gene.enrichPVal, gene.unsortedReads, gene.unsortedTotal, gene.sortedReads, gene.sortedTotal, gene.foldChange, gene.summary, gene.description]);
-        print("Printed Gene Enrichment stats")
-
 #class to hold information about each guide
 class Guide:
     def __init__(self, sequence, gene, unsortedReads, unsortedTotal, sortedReads, sortedTotal):
@@ -302,49 +139,6 @@ class Guide:
         self.adjEnrichPVal = 0;
         #store fold change
         logFoldChange = 0;
-
-#extract guides sequences and read counts from unsorted and sorted populations, and perform Fisher's exact test to determine enrichment p-value
-def calcGuideEnrich(unsortedGuideCountDict, unsortedTotMatches, sortedGuideCountDict, sortedTotMatches, guideGeneDict):
-    guideStatsDict = {}; #new dictionary to hold guide objects
-    enrichPVals = [];
-    keys = unsortedGuideCountDict.keys();
-    keys_total = len(keys)
-    for counter, key in enumerate(keys):
-        #make new guide object with the correct counts
-        sys.stdout.write("\r%d" % (counter))
-        sys.stdout.flush()
-        guide = Guide(key, guideGeneDict[key], unsortedGuideCountDict[key], unsortedTotMatches, sortedGuideCountDict[key], sortedTotMatches);
-        guideStatsDict[key] = guide;
-        oddsratio, pValue = stats.fisher_exact([[guide.unsortedReads, guide.unsortedTotal], [guide.sortedReads, guide.sortedTotal]]);
-        guide.enrichPVal = pValue;
-        enrichPVals.append(pValue);
-    #get FDR-corrected pvalues
-    adjEnrichPVals = compTool.multipletests(enrichPVals, method='fdr_bh', is_sorted=False)[1];
-    for i, key in enumerate(keys):
-        guide = guideStatsDict[key];
-        guide.adjEnrichPVal = adjEnrichPVals[i];
-
-    return guideStatsDict;
-
-#prints the enrichment p-values for each guide to a .csv file. The file is ranked from lowest FDR-corrected p-value to highest
-def printGuideEnrichStats(guideStatsDict, output_file):
-    guidesList = list(guideStatsDict.values());
-    guidesList.sort(key = lambda guide: guide.adjEnrichPVal);
-    outputName = str(output_file+"_guide_enrichment_calculation.csv");
-    with open(outputName, 'w') as csvfile:
-        mywriter = csv.writer(csvfile, delimiter=',');
-        mywriter.writerow(["Guide Sequence", "Gene", "FDR-corrected p-value", "p-value", "Reads in Unsorted Population", "Total Reads in Unsorted Population", "Reads in Sorted Population", "Total Reads in Sorted Population","","Reads in Unsorted+1","Final Total Unsorted Reads","Reads in Sorted + 1", "Final Total Sorted Reads", "Fold-change", "Log2 (Fold-change)"]);
-        plusUnsortedReads = ((guidesList[0]).unsortedTotal)+len(guidesList); #get the total number of reads in the unsorted population and add 1 additional read per guide
-        plusSortedReads = ((guidesList[0]).sortedTotal)+len(guidesList);
-        for guide in guidesList:
-            #calculate normalized fold change of each guide
-            normSorted = (guide.sortedReads+1.0)/(plusSortedReads);
-            normUnsorted = (guide.unsortedReads+1.0)/(plusUnsortedReads);
-            foldChange = normSorted/normUnsorted;
-            logFoldChange = math.log(foldChange, 2); #get log2(fold-change)
-            guide.logFoldChange = logFoldChange;
-            mywriter.writerow([guide.sequence, guide.gene, guide.adjEnrichPVal, guide.enrichPVal, guide.unsortedReads, guide.unsortedTotal, guide.sortedReads, guide.sortedTotal,"",(guide.unsortedReads+1), plusUnsortedReads, (guide.sortedReads+1), plusSortedReads, foldChange, logFoldChange]);
-        print("Printed Guide Enrichment stats");
 
 def printAnalysesInputs(guideStatsDict, output_file):
     guidesList = list(guideStatsDict.values());
@@ -418,11 +212,12 @@ def run_screen_analysis(unsorted_fastq, sorted_fastq, guides_file, output_file):
     # annotate_functions.annotateMageckFile(mageckResults, geneFuncsDict);
 
 class parseThread(threading.Thread):
-    def __init__(self, sorted, unsorted, output, guides):
+    def __init__(self, sorted, unsorted, output, guides, control):
         self.sorted = sorted
         self.unsorted = unsorted
         self.output = output
         self.guides = guides
+        self.control = control
         self.count_status = 0
         self.status = "Thread Initialized"
         print("Initializing thread information...")
@@ -436,7 +231,7 @@ class parseThread(threading.Thread):
         print("created required dictionaries");
         self.status = "Created dictionaries..."
         #count reads per guides and per gene for unsorted population if the fastq file is provided
-        print("countDict = ", countDict)
+        #print("countDict = ", countDict)
         unsortedGuideCountDict = copy.deepcopy(countDict);
         unsortedGeneCountDict = copy.deepcopy(geneCountDict);
         unsortedTotMatches = 0;
@@ -476,6 +271,16 @@ class parseThread(threading.Thread):
             self.status = "Performing enrichment analysis on guides..."
             guideStatsDict = self.calcGuideEnrich(unsortedGuideCountDict, unsortedTotMatches, sortedGuideCountDict, sortedTotMatches, guideGeneDict);
             self.printGuideEnrichStats(guideStatsDict, self.output);
+
+        self.printAnalysesInputs(guideStatsDict, self.output);
+        mageckFile = str(self.output+"_mageck_input.txt");
+        command = "mageck test -k " + mageckFile + " -t 1 -c 0 -n " + str(self.output)+"_mageck" + " --sort-criteria pos --gene-lfc-method alphamean --control-sgrna " + str(self.control)
+        print("Running command!")
+        #command = command.split(" ")
+        #print(command)
+        runcommand(command)
+
+        print("DONE!")
         self.status = "Analysis Complete"
         self.myResult = self.output
 
@@ -697,6 +502,9 @@ class parseThread(threading.Thread):
         mageckFile.close();
         print("Printed MAGeCK input files");
 
+
+def runcommand(command):
+    pro = os.system(command)
 
 #main method that ties processes together
 def main(argv):
